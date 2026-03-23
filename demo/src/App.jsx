1bce0ff6e6e63e8a5e11de96b32d42b03abc1156
cg-zhou <z_cg@foxmail.com>
2026-03-23 09:36:42 +0800

    fix bug of esa deployment

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import { Button, Card, ConfigProvider, Layout, Menu, Table, Tag, Typography } from "antd"
 import { supportSections, platformColumns } from "./supportMatrix"
 import { architectureDiagram, renderPipelineDiagram } from "./techDesign"
