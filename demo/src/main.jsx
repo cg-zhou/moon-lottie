49c6d9297f881d1873187d27fbd82629b7d13dbb
cg-zhou <z_cg@foxmail.com>
2026-03-29 20:46:28 +0800

    feat(demo): add local icons and register them in main.jsx

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -4,6 +4,19 @@ import 'antd/dist/reset.css'
 import 'iconify-icon'
 import App from './App'
 import './index.css'
+import icons from './icons.json'
+import { addIcon } from 'iconify-icon'
+
+// Load icons locally
+Object.values(icons).forEach(set => {
+  Object.entries(set.icons).forEach(([name, data]) => {
+    addIcon(`${set.prefix}:${name}`, {
+      body: data.body,
+      width: set.width,
+      height: set.height
+    });
+  });
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
